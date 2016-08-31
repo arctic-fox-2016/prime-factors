7eb6b3e02436b6b2b221dc4940851e510d4b0067
Ari Adiprana & Tevin <pair+ariadiprana.tevinstein@hacktiv8.com>
2016-08-31 19:15:29 +0700

    release 1

--- recursive.js
+++ recursive.js
@@ -3,7 +3,8 @@
 
 let prime_factors = (angka, hasil = []) => {
   //write your code here
-    let faktornya = [] //8 2<=4
+    let faktornya = []
+
     for (var i = 2; i <= angka / i; i++) {
       while (angka % i == 0) {
         faktornya.push(i);
@@ -16,19 +17,31 @@ let prime_factors = (angka, hasil = []) => {
     return faktornya;
 }
 
-
+console.log("========= release 0 =========")
 console.log(prime_factors(3));        // => [3]
 console.log(prime_factors(6));         // => [2,3]
 console.log(prime_factors(8));         // => [2,2,2]
 console.log(prime_factors(25));        // => [5,5]
 console.log(prime_factors(123123123)); // => [3, 3, 41, 333667]
-//
-// //release 1
-//
-// let simple_recursive = (number) => {
-//   //write your code here
-// }
-//
-// console.log(simple_recursive(39)); // 4
-// console.log(simple_recursive(999)); // 2
-// console.log(simple_recursive(3)); // 3
+
+//release 1
+console.log("========= release 1 =========")
+let simple_recursive = (number) => {
+  //write your code here
+  let temp = number.toString().split("")
+  let panjang = temp.length
+  let hasil = 1
+  if(panjang==1)
+  {
+      return temp[0]
+  }
+  for(var i=0 ; i<temp.length ; i++){
+    hasil *= temp[i]
+  }
+  return simple_recursive(hasil)
+
+}
+
+console.log(simple_recursive(39)); // 4
+console.log(simple_recursive(999)); // 2
+console.log(simple_recursive(3)); // 3
