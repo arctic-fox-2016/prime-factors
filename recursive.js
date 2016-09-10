52b68aa2a315aff677b9147265315d015b238461
ahyanarizky <ahyana.rizky@gmail.com>
2016-09-11 04:45:26 +0700

    untuk perbaikan nilai

--- recursive.js
+++ recursive.js
@@ -1,20 +1,38 @@
 "use strict";
 //release 0
 
-let prime_factors = (angka, hasil = []) => {
-  //write your code here
+let prime_factors = (angka) => {
+    //write your code here
+    let arr = []
+    for (var i = 1; i <= angka; i++) {
+        while (angka % i == 0 && i != 1) {
+            arr.push(i)
+            angka /= i
+        }
+    }
+    return arr
+
 }
 
-console.log(prime_factors(3));        // => [3]
-console.log(prime_factors(6));         // => [2,3]
-console.log(prime_factors(8));         // => [2,2,2]
-console.log(prime_factors(25));        // => [5,5]
+console.log(prime_factors(3)); // => [3]
+console.log(prime_factors(6)); // => [2,3]
+console.log(prime_factors(8)); // => [2,2,2]
+console.log(prime_factors(25)); // => [5,5]
 console.log(prime_factors(123123123)); // => [3, 3, 41, 333667]
 
 //release 1
 
 let simple_recursive = (number) => {
-  //write your code here
+    //write your code here
+    let nstring = number.toString()
+    let result = 1
+    if (nstring.length != 1) {
+        for (let i = 0; i < nstring.length; i++) {
+            result *= parseInt(nstring[i])
+        }
+        return simple_recursive(result)
+    }
+    return nstring
 }
 
 console.log(simple_recursive(39)); // 4
